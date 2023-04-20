86c094ba9a1ce14a92cec47616821dc55a65c093
nailaanai <nailasaniyyah81@gmail.com>
2023-04-20 13:49:38 +0700

    membenarkan konflik

--- src/BodyBTreeKamus.c
+++ src/BodyBTreeKamus.c
@@ -501,10 +501,11 @@ Address SearchTree(Address Root, String Input)
 void SearchKata(Address Tree)
 {
     char InputUser[MAX_BUFFER];
+    Address TempTree;
     printf("Masukan kosakata yang akan dicari dalam bahasa sunda : ");
     scanf(" %[^\n]", InputUser);
     InputUser[0] = toupper(InputUser[0]);
-    Address TempTree = SearchTree(Tree, InputUser);
+    TempTree = SearchTree(Tree, InputUser);
     
     if(TempTree != NULL)
     {
