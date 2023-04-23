0499cf8dd126f3c03b2dff60803a098e4749bfcb
Nailaaa <117962881+nailaanai@users.noreply.github.com>
2023-04-23 18:56:11 +0700

    menambahkan load kamus pada main.c

--- src/main.c
+++ src/main.c
@@ -1,16 +1,15 @@
 #include "BTreeKamus.h"
-#include "NonRestricted.h"
+// #include "NonRestricted.h"
 
 int main()
 {
     /* Deklarasi variabel */
-    Address KamusTree = InitTree();
-    int Exit = 0;
-
+    Address KamusTree = NULL;
+    bool Exit = false;
+    
     /* Program utama */
     // Pengaturan ukuran console dengan lebar 1000 dan tinggi 600 
-    HWND hWnd = GetConsoleWindowNT();
-    MoveWindow(hWnd, 180, 50, 1000, 600, 1);
+    LoadDataKamus(&KamusTree);
 
     /* Run */
     while(!Exit) Execute(Menu(), &KamusTree, &Exit);
